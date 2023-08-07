7ab9805b565a935becff24a22b09adc76654f594
Eyuphan Akkaya <akkayaeyupp@gmail.com>
2023-08-07 11:54:02 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Eyüphan Akkaya on 7.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
